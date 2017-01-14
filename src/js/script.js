f1488962ad28a4a5a91846b352070b08f4edc4d4
Loris Bettazza <loris.bettazza@gmail.com>
2017-01-15 00:32:29 +0100

    Added mobile hamburger menu + fixed header

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,51 @@
-(($) => {
-  $('#name').parallax({
+(($, window) => {
+  // Cache DOM
+  const $window = $(window);
+  const $html = $('html');
+  const $header = $('.header');
+  const $name = $('#name');
+  const $hamburger = $('.hamburger');
+
+  // Variables
+  let yPosition = 0;
+  const nameHeight = $name.height();
+  const headerHeight = $header.height();
+
+  // Functions
+  function toggleMenu() {
+    $html.toggleClass('menu-open');
+
+    setTimeout(() => {
+      $html.toggleClass('menu-animation');
+    }, 30);
+  }
+
+  // Event listeners
+  $window.on('scroll', function scrollHandler() {
+    yPosition = $(this).scrollTop();
+
+    if (window.matchMedia('(max-width: 767px)').matches) {
+      const opacity = yPosition / (nameHeight - headerHeight);
+      $header.css('background-color', `rgba(255, 255, 255, ${opacity})`);
+
+      if (opacity >= 1) {
+        $header.addClass('shadow');
+      } else {
+        $header.removeClass('shadow');
+      }
+    } else {
+      $header.css('background-color', '');
+    }
+  });
+
+  $hamburger.on('click', toggleMenu);
+
+  // Body
+  $window.scroll();
+
+  $name.parallax({
     imageSrc: '/img/parallax-mountains.jpg',
     speed: 0.5,
     position: 'center bottom',
   });
-})(jQuery);
+})(jQuery, window); // eslint-disable-line no-undef
